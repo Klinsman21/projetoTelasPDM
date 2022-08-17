44a193d2bd045caa89e92f026b843b97a1200571
klinsman <joseklinsman259@gmail.com>
2022-08-17 11:17:22 -0300

    Convertendo para react native CLI

--- App.js
+++ App.js
@@ -4,17 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Olá mundo</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    position: "absolute",
+    // flex: 1,
+    justifyContent: "center",
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    backgroundColor: 'rgba(255, 148, 0, 1)',
+    width: 428,
+    height: 442,
+    left: 0,
+    top: 0,
   },
+  title: {
+    position: "relative",
+    top: 80,
+    textAlign: "center",
+    fontSize: 36,
+
+  }
 });
